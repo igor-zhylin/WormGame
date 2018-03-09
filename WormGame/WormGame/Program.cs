ac66be7f4d28d9cf3e5d1aa8c9b227d366bab9af
igor-zhylin <37139257+igor-zhylin@users.noreply.github.com>
2018-03-09 19:37:23 +0200

    EmptyProject

---
+++ WormGame/WormGame/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace WormGame
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
